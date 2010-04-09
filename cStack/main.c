f97d1448b98565964d544cd5326557a2373042f9
Fredrik Wallgren <fredrik.wallgren@gmail.com>
2010-04-10 00:21:15 +0200

    Add test for stack

--- cStack/main.c
+++ cStack/main.c
@@ -8,13 +8,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "Stack.h"
+
 /*
  * 
  */
 int main(int argc, char** argv)
 {
-  printf("Hello World!\n");
+  printf("Stack test\n");
+
+  int result;
+
+  Stack s;
+  StackNew(&s, sizeof(int));
+
+  int i = 0;
   
+  for(i; i < 100; i += 1)
+  {
+    StackPush(&s, &i);
+  }
+
+  StackPop(&s, &result);
+  printf("top = %i\n", result);
+
+  i = 200;
+  for(i; i < 300; i += 1)
+  {
+    StackPush(&s, &i);
+  }
+
+  i = 0;
+  for(i; i < 100; i += 1)
+  {
+    StackPop(&s, &result);
+    printf("top = %i\n", result);
+  }
+
+  StackPeek(&s, &result);
+  printf("top = %i\n", result);
+
+  StackPeek(&s, &result);
+  printf("top = %i\n", result);
+
+  StackPeek(&s, &result);
+  printf("top = %i\n", result);
+
+  printf("Done!\n");
+
   return (EXIT_SUCCESS);
 }
 
