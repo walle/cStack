6192d1dfc7cbaf8c71d7ca55013e85c6b8b0432e
Fredrik Wallgren <fredrik.wallgren@gmail.com>
2010-04-09 19:49:23 +0200

    Add hello world project

---
+++ cStack/main.c
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.c
+ * Author: walle
+ *
+ * Created on April 9, 2010, 7:44 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+  printf("Hello World!\n");
+  
+  return (EXIT_SUCCESS);
+}
+
